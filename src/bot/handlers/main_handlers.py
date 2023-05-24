72c6a1ad6498ca3f771f9b41856e410e0ae61fa6
Ubuntu 20.04 <morgin95@mail.ru>
2023-05-24 06:53:41 -0700

    Started doing handlers

--- src/bot/handlers/main_handlers.py
+++ src/bot/handlers/main_handlers.py
@@ -1,13 +1,26 @@
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes, CommandHandler
 
+from bot.constants import HELP_MESSAGE, START_MESSAGE
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+ONE, TWO, THREE, FOUR = range(4)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Точка старта бота. Приветствие. Две кнопки."""
+    keyboard = [
+        [
+            InlineKeyboardButton("1", callback_data=str(ONE)),
+            InlineKeyboardButton("2", callback_data=str(TWO)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(START_MESSAGE, reply_markup=reply_markup)
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+    """Функция показывает информацию о том, как использовать этот бот"""
+    await update.message.reply_text(HELP_MESSAGE)
 
 start_handler = CommandHandler('start', start)
 help_handler = CommandHandler('help', help_command)
--- src/run_bot.py
+++ src/run_bot.py
@@ -1,11 +1,31 @@
-from telegram.ext import ApplicationBuilder
+from telegram.ext import (
+    Application,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+)
 
 from bot.handlers.main_handlers import start_handler, help_handler
 from bot.core.config import settings
 
+START_ROUTES, END_ROUTES = range(2)
+
 
 def main():
-    app = ApplicationBuilder().token(settings.telegram_token).build()
+    app = Application.builder().token(settings.telegram_token).build()
+    #conv_handler = ConversationHandler(
+    #    entry_points=[CommandHandler("start", start_handler)],
+    #    states={
+    #        START_ROUTES: [
+    #            CommandHandler("help", help_handler)
+    #        ],
+    #    },
+    #    fallbacks=[
+    #        CommandHandler("start", start_handler),
+    #        CommandHandler("help", help_handler)
+    #    ]
+    #)
     app.add_handlers([start_handler, help_handler])
     app.run_polling()
 
