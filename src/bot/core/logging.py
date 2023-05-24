ecb99928809fc9892e8aeba44bcbec359a0a2642
Konstantin Raikhert <69113745+KonstantinRaikhert@users.noreply.github.com>
2023-05-24 22:38:41 +0300

    Revert "Настройка логов (#6)"
    This reverts commit 3a273d194567291a9749ed16669e100ddacd8020.

--- src/bot/core/logging.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__name__).parent
-
-
-log_dir = BASE_DIR / 'logs'
-log_dir.mkdir(exist_ok=True)
-log_file = log_dir / 'bot.log'
--- src/run_bot.py
+++ src/run_bot.py
@@ -1,16 +1,10 @@
-import logging.config
-
 from telegram.ext import ApplicationBuilder
 
+from bot.handlers.main_handlers import start_handler, help_handler
 from bot.core.config import settings
-from bot.handlers.main_handlers import help_handler, start_handler
-from src.bot.core.log_config import LOGGING_CONFIG
 
 
 def main():
-    logging.config.dictConfig(LOGGING_CONFIG) 
-    logger = logging.getLogger('bot')
-    logger.info('start')
     app = ApplicationBuilder().token(settings.telegram_token).build()
     app.add_handlers([start_handler, help_handler])
     app.run_polling()
