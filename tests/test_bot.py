1d71cad3fd8b4d7d84aec61006db30585dcbc75a
Vlad <95340858+grachevvladislav@users.noreply.github.com>
2023-05-30 18:40:11 +0300

    Feature/fix donate button (#20)
    * remove donation state
    
    * remove donation state
    
    * fix flake8

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,14 +1,10 @@
 # Здесь хранятся тесты бота
 import pytest
-
-from telegram import (
-    Bot,
-)
+from telegram import Bot
 from telegram.error import InvalidToken
 
 
 class TestBotWithoutRequest:
-
     async def test_no_token_passed(self):
         with pytest.raises(InvalidToken, match="You must pass the token"):
             Bot("")
