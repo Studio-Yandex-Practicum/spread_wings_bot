259292358a5aeb2316c20a192c97c25851c45698
Ubuntu 20.04 <morgin95@mail.ru>
2023-06-02 01:19:13 -0700

    Добавил аннотацию и заменил срез строки на replace

--- src/bot/constants/back_functions.py
+++ src/bot/constants/back_functions.py
@@ -1,8 +1,10 @@
+from typing import Any, Awaitable, Callable
+
 from bot.constants.states import States
 from bot.handlers.assistance import receive_assistance
 from bot.handlers.main_handlers import start
 
-FUNCTIONS = {
+FUNCTIONS: dict[str, Callable[[Any, Any], Awaitable[States]]] = {
     States.REGION.value: receive_assistance,
     States.ASSISTANCE.value: start,
 }
