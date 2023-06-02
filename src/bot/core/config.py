5aee9c2ae21751bb9198e93df802af9ca36c9b12
Aleksandr <95490984+rtx4090@users.noreply.github.com>
2023-06-03 04:02:55 +0700

    Коннект с базой (#32)
    * condb
    
    * condb0
    
    * condb0

--- src/bot/core/config.py
+++ src/bot/core/config.py
@@ -1,13 +1,19 @@
+from dotenv import find_dotenv
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    """Settings app."""
+
+    db_url: str
     telegram_token: str
     debug: bool = False
 
     class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+        """Env settings."""
+
+        env_file = find_dotenv(".env")
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()
