04b43893c79aaa4de4daa42590520c14a5101966
rtx4090 <a.kopneev@gmail.com>
2023-06-02 23:49:27 +0700

    condb0

--- src/bot/core/config.py
+++ src/bot/core/config.py
@@ -1,17 +1,18 @@
+from dotenv import find_dotenv
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     """Settings app."""
 
-    db_url: str = "mysql+aiomysql://root:YP_2023-05@jetrai.online/krilya_dets1"
+    db_url: str
     telegram_token: str
     debug: bool = False
 
     class Config:
         """Env settings."""
 
-        env_file = ".env"
+        env_file = find_dotenv(".env")
         env_file_encoding = "utf-8"
 
 
--- src/bot/db/db.py
+++ src/bot/db/db.py
@@ -1,6 +1,3 @@
-import asyncio
-
-from sqlalchemy import text
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
@@ -9,23 +6,9 @@ from sqlalchemy.ext.asyncio import (
 
 from src.bot.core.config import settings
 
-loop = asyncio.get_event_loop()
 
-engine = create_async_engine(settings.db_url)
-async_session = async_sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
-
-
-async def exp_session():
-    """For example."""
-    try:
-        async with async_session() as session:
-            sql = await session.execute(text("SHOW TABLES"))
-            result = sql.scalars().all()
-            return f"conect ok: {result}"
-    except Exception as e:
-        print(f"ops: {e}")
-
-
-print(loop.run_until_complete(exp_session()))
+async def start_session():
+    """Async connect with db."""
+    engine = create_async_engine(settings.db_url)
+    session = async_sessionmaker(engine, class_=AsyncSession)
+    return await session()
