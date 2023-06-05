35c36be6fa0e9667070b93558d0a3b6f698325e5
LIK <you-2-me@yandex.ru>
2023-06-05 23:02:09 +0300

    Add back handler

---
+++ src/bot/handlers/back_handler.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from bot.constants.back_functions import FUNCTIONS
+from bot.constants.states import States
+
+
+async def back_button(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> States:
+    """Возврат в предыдущее состояние."""
+    query = update.callback_query
+    command = query.data.replace("back_to_", "")
+    await FUNCTIONS.get(command)(update, context)
+    return command
