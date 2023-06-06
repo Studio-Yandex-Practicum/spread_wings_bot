1dbeacae735b39907fd4647acd3750ffb9b2c04a
rtx4090 <a.kopneev@gmail.com>
2023-06-06 23:15:02 +0700

    aiomysqldsn

--- src/bot/core/config.py
+++ src/bot/core/config.py
@@ -1,11 +1,17 @@
 from dotenv import find_dotenv
-from pydantic import BaseSettings, SecretStr
+from pydantic import AnyUrl, BaseSettings, SecretStr
+
+
+class CustomDsn(AnyUrl):
+    """aiomysqlDSN."""
+
+    allowed_schemes = {"mysql+aiomysql"}
 
 
 class Settings(BaseSettings):
     """Settings app."""
 
-    db_url: str
+    db_url: CustomDsn
     telegram_token: SecretStr
     debug: bool = False
 
