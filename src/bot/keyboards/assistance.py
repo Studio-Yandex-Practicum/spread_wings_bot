d867996c812f1065ffbb906e13ac49c28bfdae46
Leonid <103986216+lik-me@users.noreply.github.com>
2023-06-06 23:11:47 +0300

    Feature/connect with us (#33)
    * Add handler contact with us
    
    * Edit handler contact with us
    
    * Edit handler contact with us
    
    * Final edit of handler connect_with_us
    
    * Fixing last changes
    
    * Add docstrings to fixture functions
    
    * Add docstrings to fixture functions
    
    * Update .pre-commit-config.yaml
    
    * Add back handler
    
    * Add back handler
    
    * Add back handler
    
    * Add back handler
    
    * Edit .pre-commit-config.yaml

--- src/bot/keyboards/assistance.py
+++ src/bot/keyboards/assistance.py
@@ -3,7 +3,9 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from bot.constants.buttons import (
     ASSISTANCE_BUTTON,
     BACK_BUTTON,
+    CONTACTS,
     DONATION_BUTTON,
+    SEND_REQUEST,
 )
 from bot.constants.regions import Regions
 from bot.constants.states.main_states import States
@@ -22,6 +24,7 @@ region_keyboard = [
     [InlineKeyboardButton(region.value, callback_data=region.name)]
     for region in Regions
 ]
+
 region_keyboard.append(
     [
         InlineKeyboardButton(
@@ -30,5 +33,22 @@ region_keyboard.append(
     ]
 )
 
+contact_keyboard = [
+    [
+        InlineKeyboardButton(SEND_REQUEST, callback_data=States.BACK.value),
+        InlineKeyboardButton(
+            CONTACTS, callback_data=States.SHOW_CONTACTS.value
+        ),
+    ],
+    [InlineKeyboardButton(BACK_BUTTON, callback_data=States.REGION.value)],
+]
+
+contact_show_keyboard = [
+    [InlineKeyboardButton(BACK_BUTTON, callback_data=States.CONTACT_US.value)]
+]
+
+
 assistance_keyboard_markup = InlineKeyboardMarkup(assistance_keyboard)
 region_keyboard_markup = InlineKeyboardMarkup(region_keyboard)
+contact_keyboard_markup = InlineKeyboardMarkup(contact_keyboard)
+contact_show_keyboard_markup = InlineKeyboardMarkup(contact_show_keyboard)
