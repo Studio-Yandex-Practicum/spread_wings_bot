fff1dd8b9ebc7ba265db35194ff1b352f9446347
Алия Бисенгалиева <105454189+AliaBis@users.noreply.github.com>
2023-06-10 21:09:42 +0300

    задача кнопки получить помощь

--- src/bot/constants/buttons.py
+++ src/bot/constants/buttons.py
@@ -4,12 +4,13 @@ CONTACT_US_BUTTON = "Связаться с нами"
 
 # Кнопки для блока "Помочь или получить помощь"
 ASSISTANCE_BUTTON = "Получить помощь"
+
 DONATION_BUTTON = "Сделать пожертвование"
 
 # Кнопки для блока "Чем мы можем помочь"
-LEGAL_HELP_BUTTON = "Юридическая помощь"
-SOCIAL_HELP_BUTTON = "Социальная помощь"
-PSYCHOLOGICAL_HELP_BUTTON = "Психологическая помощь"
+LEGAL_HELP_BUTTON = "Юристы"
+SOCIAL_HELP_BUTTON = "Соцработники"
+PSYCHOLOGICAL_HELP_BUTTON = "Психологи"
 PROGRAMS_BUTTON = "Наши программы"
 ASK_QUESTION = "Задать свой вопрос"
 
