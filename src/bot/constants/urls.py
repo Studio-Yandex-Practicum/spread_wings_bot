68ef062209d8d4c17d36f025ef55cf703756100f
Nikita Blokhin <nblohin97@gmail.com>
2023-06-15 21:50:06 +1000

    добавлено извлечеие таблицы контактов из бд и обновлен парсер контаков из таблицы

--- src/bot/constants/urls.py
+++ src/bot/constants/urls.py
@@ -1,2 +1,1 @@
 DONATION_URL = "https://detskyfond.info/pomogi/"
-CONTACTS_URL = "http://jetrai.online/kontakty-koordinatorov/"
--- src/bot/parsing_contacts.py
+++ src/bot/parsing_contacts.py
@@ -1,25 +1,27 @@
-from pprint import pprint
-
-import requests_cache
 from bs4 import BeautifulSoup
-from constants.urls import CONTACTS_URL
+from db.db import start_session
+from sqlalchemy import text
 
 
-def parser() -> None:
-    """Парсер контактов кураторов."""
-    session = requests_cache.CachedSession()
-    response = session.get(CONTACTS_URL)
-    response.encoding = "utf-8"
-    results = []
-    soup = BeautifulSoup(response.text, features="lxml")
-    body_tag = soup.body
-    div_container_tag = body_tag.find("div", attrs={"class": "container"})
-    div_content_tag = div_container_tag.find(
-        "div", attrs={"class": "content-wrap"}
+async def extract_data_from_db():
+    """Извлчение html таблицы контактов координаторов из БД."""
+    session = start_session()
+    coordinators = await session.execute(
+        text(
+            "SELECT post_content FROM detfond_posts WHERE post_name IN"
+            '("kontakty-koordinatorov")'
+        )
     )
-    art_hide_tag = div_content_tag.find("article", attrs={"class": "autohide"})
-    div_art_tag = art_hide_tag.find("div", attrs={"class": "article-text"})
-    tbody_tag = div_art_tag.find("tbody")
+    coordinators = coordinators.scalars().first()
+    await session.close()
+    return coordinators
+
+
+def parser_coordinators(data) -> None:
+    """Парсер контактов коондинаторов из таблицы html."""
+    results = {"results": []}
+    soup = BeautifulSoup(data, features="lxml")
+    tbody_tag = soup.find("tbody")
     tr_tags = tbody_tag.find_all("tr")
     attributes_dict = [tr_tag.text for tr_tag in tr_tags[0]][1::2]
     for tr_tag in tr_tags[1:]:
@@ -29,8 +31,5 @@ def parser() -> None:
         for td_tag in td_tags:
             result[attributes_dict[index]] = td_tag.text
             index += 1
-        results.append(result)
-    pprint(results)
-
-
-parser()
+        results["results"].append(result)
+    return results
