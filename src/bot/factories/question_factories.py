2f40d444872f391b39e9c7ff919e51a4debd2f67
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-07-11 18:03:49 +0300

    no more db in dactories, just html-generator

--- src/bot/factories/question_factories.py
+++ src/bot/factories/question_factories.py
@@ -1,18 +1,12 @@
 import argparse
-import asyncio
 import os
 import random
 import sys
 
 from bs4 import BeautifulSoup
 from factory import Factory, Faker, SubFactory
-from sqlalchemy import Column, select
-from sqlalchemy.dialects.mysql import BIGINT, DATETIME, LONGTEXT, TEXT, VARCHAR
-from sqlalchemy.orm import declarative_base
 
 from src.bot.constants.regions import Regions
-from src.bot.core.exceptions import PostNotFound
-from src.bot.db.db import start_session
 from src.bot.factories.service import generate_dict_factory
 
 
@@ -93,63 +87,10 @@ class QuestionFundFactory(Factory):
 
 factory_to_dict = generate_dict_factory(QuestionFactory)
 
-Base = declarative_base()
-
-
-class PostModel(Base):
-    """
-    Модель постов из бд WordPress на диаклекте MYSQL.
-
-    Все поля - чтобы не ломать посты.
-    """
-
-    __tablename__ = "detfond_posts"
-    ID = Column(BIGINT, primary_key=True)
-    post_author = Column(BIGINT)
-    post_date = Column(DATETIME)
-    post_date_gmt = Column(DATETIME)
-    post_content = Column(LONGTEXT)
-    post_title = Column(TEXT)
-    post_excerpt = Column(TEXT)
-    post_status = Column(VARCHAR(20))
-    comment_status = Column(VARCHAR(20))
-    ping_status = Column(VARCHAR(20))
-    post_password = Column(VARCHAR(255))
-    post_name = Column(VARCHAR(200))
-    to_ping = Column(TEXT)
-    pinged = Column(TEXT)
-    post_modified = Column(DATETIME)
-    post_modified_gmt = Column(DATETIME)
-    post_content_filtered = Column(LONGTEXT)
-    post_parent = Column(BIGINT)
-    guid = Column(VARCHAR(255))
-    menu_order = Column(BIGINT)
-    post_type = Column(VARCHAR(20))
-    post_mime_type = Column(VARCHAR(100))
-    comment_count = Column(BIGINT)
-
-
-QST_TYPES_TO_POST_ID = {
-    "psycho": 17016,
-    "social": 17018,
-    "legal": 17020,
-}
-
-
-async def get_post(post_id, session):
-    """Находим нужный нам пост."""
-    post = await session.execute(
-        select(PostModel).where(
-            PostModel.ID == post_id,
-            PostModel.post_status == "publish",
-        )
-    )
-    return post.scalars().first()
-
 
 def get_random_region():
     """Генерим случайные значения для региона из enum."""
-    OPTIONS = [
+    options = [
         random.choice(list(Regions)).value,
         f"{random.choice(list(Regions)).value}, {random.choice(list(Regions)).value}",
         f"{random.choice(list(Regions)).value}, "
@@ -157,18 +98,18 @@ def get_random_region():
         "",
         " ",
     ]
-    city = random.choice(OPTIONS)
+    city = random.choice(options)
     return city
 
 
-def parse_and_recreate_record(record, question_cnt):
+def parse_and_recreate_record(file, question_cnt):
     """
     Отвратительная Черная Магия.
 
     Парсим html-контент поста, подменяем нужные поля согласно
     заданному и имеющемся кол-ву вопросов.
     """
-    soup = BeautifulSoup(record.post_content, features="lxml")
+    soup = BeautifulSoup(file, features="lxml")
     table = soup.find("table")
     trs = table.find_all("tr")
     for i in range(len(trs)):
@@ -199,23 +140,16 @@ def parse_and_recreate_record(record, question_cnt):
     return soup.find("table")
 
 
-async def insert_questions_to_db(data, record, session):
-    """Записываем полученный пост в базу и защищаем закрытие сессии."""
-    record.post_content = data
-    session.add(record)
-    await session.commit()
-    await asyncio.shield(session.close())
-
-
-async def generate_questions_to_db(qst_cnt, qst_type):
+def generate_questions(qst_cnt):
     """Открываем сессию, получаем пост, корректируем и пишем в бд."""
-    session = await start_session()
-    post = await get_post(QST_TYPES_TO_POST_ID[qst_type], session)
-    if post:
-        data_to_db = parse_and_recreate_record(post, qst_cnt)
-        await insert_questions_to_db(data_to_db, post, session)
-    else:
-        raise PostNotFound(f"Пост типа {qst_type} не найден в базе")
+    new_data = None
+
+    with open("example.html") as file:
+        post = file.read()
+        new_data = str(parse_and_recreate_record(post, qst_cnt))
+
+    with open("result.html", "w+") as new_file:
+        new_file.write(new_data)
 
 
 def read_arg():
@@ -233,20 +167,8 @@ def read_arg():
         default=10,
         help="Количество вопросов для генерации",
     )
-    keys = list(QST_TYPES_TO_POST_ID.keys())
-    parser.add_argument(
-        "-type",
-        choices=keys,
-        default=None,
-        help="Количество вопросов для генерации",
-    )
     args = parser.parse_args()
-    if args.type is None:
-        print("Не выбран тип вопросов (-type psycho | social | legal)")
-        return
-    asyncio.get_event_loop().run_until_complete(
-        generate_questions_to_db(args.count, args.type)
-    )
+    generate_questions(args.count)
 
 
 if __name__ == "__main__":
