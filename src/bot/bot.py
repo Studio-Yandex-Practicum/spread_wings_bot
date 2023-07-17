e851d530e860456ab843cbc897ea6604c760d842
Aleksei Koznov <102172028+alekseikoznov@users.noreply.github.com>
2023-07-17 15:52:26 +0300

    changed regex (#80)

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -116,13 +116,13 @@ def build_app() -> Application:
     )
     ask_question_handler = ConversationHandler(
         entry_points=[
-            MessageHandler(filters.Regex("^.*$"), get_question),
+            MessageHandler(filters.Regex(r"^[^\/].*$"), get_question),
         ],
         persistent=True,
         name="ask_question_handler",
         states={
             AskQuestionStates.QUESTION: [
-                MessageHandler(filters.Regex("^.*$"), get_name),
+                MessageHandler(filters.Regex(r"^[^\/].*$"), get_name),
             ],
             AskQuestionStates.NAME: [
                 CallbackQueryHandler(
