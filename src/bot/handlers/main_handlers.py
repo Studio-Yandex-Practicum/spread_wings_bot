87d267707099dfcca2c1d5b6dbae429702eba1e3
Alexander Lonskiy <102410304+distemper17@users.noreply.github.com>
2023-07-21 22:31:29 +0200

    Feature/upmenu delete (#107)
    * removed upmenu handler, merged functionality with start handler
    
    * removed depricated menu_handler import
    
    ---------
    
    Co-authored-by: Alexander Lonskiy <alexanderlonskiy@MBP-Alexander.netis.cc>

--- src/bot/handlers/main_handlers.py
+++ src/bot/handlers/main_handlers.py
@@ -1,6 +1,7 @@
-from telegram import Update
+from telegram import MenuButtonCommands, Update
 from telegram.ext import CommandHandler, ContextTypes
 
+from bot.constants.commands import COMMANDS
 from bot.constants.messages import HELP_MESSAGE, START_MESSAGE
 from bot.constants.states.main_states import States
 from bot.keyboards.assistance import assistance_keyboard_markup
@@ -8,6 +9,12 @@ from bot.keyboards.assistance import assistance_keyboard_markup
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> States:
     """Точка старта бота. Приветствие. Две кнопки."""
+    bot_commands = await context.bot.get_my_commands()
+    if not bot_commands:
+        await context.bot.set_my_commands(commands=COMMANDS)
+        await context.bot.set_chat_menu_button(
+            menu_button=MenuButtonCommands()
+        )
     if update.message is not None:
         await update.message.reply_text(
             START_MESSAGE, reply_markup=assistance_keyboard_markup
