c837b4a970ad2b18081253d440f14b0329bf4d50
Alexander Lonskiy <alexanderlonskiy@MBP-Alexander.netis.cc>
2023-07-21 14:49:24 +0200

    removed upmenu handler, merged functionality with start handler

--- src/bot/handlers/service_handlers.py
+++ src/bot/handlers/service_handlers.py
@@ -1,17 +1,7 @@
-from telegram import MenuButtonCommands, Update
-from telegram.ext import CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update
+from telegram.ext import ContextTypes, MessageHandler, filters
 
-from bot.constants.commands import COMMANDS
-from bot.constants.messages import ANSWER_TO_USER_MESSAGE, MENU_MESSAGE
-
-
-async def update_menu(
-    update: Update, context: ContextTypes.DEFAULT_TYPE
-) -> None:
-    """Функция обновляет список команд и кнопку меню."""
-    await context.bot.set_my_commands(commands=COMMANDS)
-    await context.bot.set_chat_menu_button(menu_button=MenuButtonCommands())
-    await update.message.reply_text(MENU_MESSAGE)
+from bot.constants.messages import ANSWER_TO_USER_MESSAGE
 
 
 async def answer_all_messages(
@@ -22,5 +12,4 @@ async def answer_all_messages(
     await update.message.reply_text(ANSWER_TO_USER_MESSAGE)
 
 
-menu_handler = CommandHandler("upmenu", update_menu)
 answer_all_messages_handler = MessageHandler(filters.ALL, answer_all_messages)
