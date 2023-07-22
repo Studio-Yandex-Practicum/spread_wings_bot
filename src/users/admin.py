c362d8435bc7dc6bddbb080a5702ad0c22c15ca5
Андрей Жирихин <zhirikhin.andrew@gmail.com>
2023-07-22 13:49:23 +0300

    delete username field from User model

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,13 +1,39 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    """User management via admin panel."""
+class UserAdmin(BaseUserAdmin):
+    """Custom user management via admin panel."""
 
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
     list_display = (
         "id",
         "first_name",
@@ -17,4 +43,8 @@ class CustomUserAdmin(UserAdmin):
         "is_staff",
         "role",
     )
-    # list_filter = ("region")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
+
+
+admin.site.register(User, UserAdmin)
--- src/users/models.py
+++ src/users/models.py
@@ -1,46 +1,43 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-ADMIN = "main_admin"
-REGION_ADMIN = "region_admin"
-
-ROLES = (
-    (ADMIN, "Администратор"),
-    (REGION_ADMIN, "Региональный администратор"),
-)
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     """User's model."""
 
+    class UserRole(models.TextChoices):
+        """Supporting model to make Choices field."""
+
+        ADMIN = "main_admin", _("Администратор")
+        REGION_ADMIN = "region_admin", _("Региональный администратор")
+
+    username = None
     first_name = models.CharField(
         max_length=50, verbose_name="Имя пользователя"
     )
     last_name = models.CharField(
         max_length=50, verbose_name="Фамилия пользователя"
     )
-    username = models.CharField(
-        max_length=150, verbose_name="Ник пользователя", null=True
-    )
     email = models.EmailField(
         unique=True, verbose_name="Адрес электронной почты"
     )
     role = models.CharField(
         max_length=12,
-        choices=ROLES,
-        default="region_admin",
+        choices=UserRole.choices,
+        default=UserRole.REGION_ADMIN,
         verbose_name="Роль пользователя",
     )
     # TODO: add Region model into django
-    # region = other_models.ForeignKey(
+    # region = models.ForeignKey(
     #     Region,
-    #     on_delete=other_models.SET_NULL,
+    #     on_delete=models.SET_NULL,
     #     null=True,
     #     verbose_name="Регион пользователя"
     # )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}, {self.role}"
