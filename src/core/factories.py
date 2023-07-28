dc8ac0683f9cc74c1324d62f10f32ce1cfbe8cf1
Алексей Кознов <alekseikoznov@yandex.ru>
2023-07-28 15:50:11 +0300

    Добавил ввод количества данных фабрик

--- src/core/factories.py
+++ src/core/factories.py
@@ -9,4 +9,4 @@ class RegionFactory(DjangoModelFactory):
         model = Region
 
     region_name = Faker('region', locale='ru_RU')
-    region_key = Faker("text", max_nb_chars=200)
+    region_key = Faker("text", max_nb_chars=20)
--- src/core/management/commands/filldb.py
+++ src/core/management/commands/filldb.py
@@ -11,6 +11,11 @@ from bot.models import (Coordinator,
                         FundProgram,
                         Question)
 
+REGION_COUNT = int(input("Необходимое количество регионов: "))
+COORDINATOR_COUNT = int(input("Необходимое количество координаторов: "))
+PROGRAM_COUNT = int(input("Необходимое количество программ: "))
+QUESTION_COUNT = int(input("Необходимое количество вопросов: "))
+
 
 class Command(BaseCommand):
     help = 'Fill the database with test data'
@@ -23,7 +28,7 @@ class Command(BaseCommand):
         Region.objects.all().delete()
         self.stdout.write("Creating new data...")
         regions = []
-        for _ in range(5):
+        for _ in range(REGION_COUNT):
             while True:
                 try:
                     region = RegionFactory()
@@ -33,10 +38,16 @@ class Command(BaseCommand):
                     regions.append(region)
                     break
 
-        for _ in range(5):
+        for _ in range(COORDINATOR_COUNT):
             region = choice(regions)
             CoordinatorFactory(region=region)
+
+        for _ in range(PROGRAM_COUNT):
+            region = choice(regions)
             fund_program = FundProgramFactory()
             fund_program.regions.add(region)
+
+        for _ in range(QUESTION_COUNT):
+            region = choice(regions)
             question = QuestionFactory()
             question.regions.add(region)
