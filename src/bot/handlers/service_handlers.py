34d8aa9916f48b6533f1c0079559735d3e1ae1c2
Alexander Lonskiy <alexanderlonskiy@MBP-Alexander.netis.cc>
2023-07-31 18:19:10 +0200

    some changes to make it work

--- src/bot/handlers/service_handlers.py
+++ src/bot/handlers/service_handlers.py
@@ -1,7 +1,6 @@
 import logging
 
 from telegram import Update
-from telegram.error import TelegramError
 from telegram.ext import ContextTypes, MessageHandler, filters
 
 from bot.constants.messages import ANSWER_TO_USER_MESSAGE
@@ -14,26 +13,27 @@ async def answer_all_messages(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
     """Удаление и ответ на любое сообщение пользователя."""
-    # Удаляем сообщение пользователя
     await update.message.delete()
 
-    # Достаем последний ID сообщения бота
     user_message_id = update.message.message_id
+    print(user_message_id)
     bot_last_message_id = user_message_id - 1
+    print(bot_last_message_id)
 
-    try:
-        message = await context.bot.edit_message_text(
-            text=ANSWER_TO_USER_MESSAGE,
-            chat_id=update.effective_chat.id,
-            message_id=bot_last_message_id,
-            reply_markup=get_back_keyboard(),
-        )
-    except TelegramError as error:
-        logger.error("Error while editing message: %s", error)
-    else:
-        # Обновляем последнее сообщение с ID = сообщению бота
-        context.user_data["last_bot_message_id"] = message.message_id
+    if bot_last_message_id:
+        try:
+            previous_state = context.user_data.get("previous_state")
+
+            await context.bot.edit_message_text(
+                ANSWER_TO_USER_MESSAGE,
+                chat_id=update.effective_chat.id,
+                message_id=bot_last_message_id,
+                reply_markup=get_back_keyboard(previous_state),
+            )
+        except Exception as e:
+            logger.error(f"Failed to edit message: {e}")
+
+    context.user_data["last_bot_message_id"] = bot_last_message_id
 
 
-# Определяем хендлер ответа на сообщения пользователя
 answer_all_messages_handler = MessageHandler(filters.ALL, answer_all_messages)
--- src/bot/keyboards/get_back.py
+++ src/bot/keyboards/get_back.py
@@ -1,18 +1,15 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot.constants.buttons import BACK_BUTTON
-from bot.constants.states.main_states import States
 
 
-def get_back_keyboard() -> InlineKeyboardMarkup:
-    """Клавиатура кнопки назад."""
-    return InlineKeyboardMarkup(
+def get_back_keyboard(previous_state=None) -> InlineKeyboardMarkup:
+    """Build the inline keyboard with a 'Back' button."""
+    back_button_inline_keyboard = [
         [
-            [
-                InlineKeyboardButton(
-                    text=BACK_BUTTON,
-                    callback_data=f"back_to_{States.ASSISTANCE.value}",
-                )
-            ],
+            InlineKeyboardButton(
+                BACK_BUTTON, callback_data=f"back_to_{previous_state}"
+            )
         ]
-    )
+    ]
+    return InlineKeyboardMarkup(back_button_inline_keyboard)
