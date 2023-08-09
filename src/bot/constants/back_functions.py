b307312ed545e019702542508f19ab0c5aaf643f
Zhirikhin Andrew <zhirikhin.andrew@gmail.com>
2023-08-09 22:09:25 +0300

    refactor of bot.py patterns

--- src/bot/constants/back_functions.py
+++ src/bot/constants/back_functions.py
@@ -4,8 +4,8 @@ from bot.constants.states.main_states import States
 from bot.handlers.assistance import (
     contact_with_us,
     receive_assistance,
+    select_assistance,
     select_type_of_help,
-    selected_type_assistance,
     show_contact,
 )
 from bot.handlers.main_handlers import start
@@ -13,7 +13,7 @@ from bot.handlers.main_handlers import start
 FUNCTIONS: dict[str, Callable[[Any, Any], Awaitable[States]]] = {
     States.SHOW_CONTACT: show_contact,
     States.CONTACT_US: contact_with_us,
-    States.QUESTIONS_AND_CONTACTS: selected_type_assistance,
+    States.QUESTIONS_AND_CONTACTS: select_assistance,
     States.ASSISTANCE_TYPE: select_type_of_help,
     States.REGION: receive_assistance,
     States.ASSISTANCE.value: start,
