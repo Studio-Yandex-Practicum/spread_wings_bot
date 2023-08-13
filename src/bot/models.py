7887fc5e538f6487e833791043451d03f319a70f
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-08-13 23:30:49 +0300

    Рефакторинг: добавлены док-стринги, форматирование строк приведено к единому типу

--- src/bot/models.py
+++ src/bot/models.py
@@ -6,25 +6,38 @@ from bot.validators import phone_regex, telegram_regex
 
 
 class Coordinator(BaseModel):
+    """Region coordinator model."""
 
-    first_name = models.CharField(max_length=200, verbose_name='Имя')
-    last_name = models.CharField(max_length=200, verbose_name='Фамилия')
-    region = models.OneToOneField(Region,
-                                  on_delete=models.PROTECT,
-                                  primary_key=True,
-                                  related_name='coordinators',
-                                  verbose_name='Регион')
-    email_address = models.EmailField(unique=True, verbose_name='Email')
-    phone_number = models.CharField(max_length=20,
-                                    unique=True,
-                                    validators=[phone_regex],
-                                    blank=True,
-                                    verbose_name='Номер телефона')
-    telegram_account = models.CharField(max_length=32,
-                                        unique=True,
-                                        validators=[telegram_regex],
-                                        blank=True,
-                                        verbose_name='Telegram')
+    first_name = models.CharField(
+        max_length=200,
+        verbose_name="Имя",
+    )
+    last_name = models.CharField(
+        max_length=200,
+        verbose_name="Фамилия",
+    )
+    region = models.OneToOneField(
+        Region,
+        on_delete=models.PROTECT,
+        primary_key=True,
+        related_name="coordinators",
+        verbose_name="Регион",
+    )
+    email_address = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[phone_regex],
+        blank=True,
+        verbose_name="Номер телефона",
+    )
+    telegram_account = models.CharField(
+        max_length=32,
+        unique=True,
+        validators=[telegram_regex],
+        blank=True,
+        verbose_name="Telegram",
+    )
 
     class Meta:
         verbose_name = "Координатор"
@@ -33,7 +46,7 @@ class Coordinator(BaseModel):
 
 
 class HelpTypes(models.TextChoices):
-    """Supporting model to make Choices field."""
+    """Supporting model to make choices field."""
 
     LEGAL_ASSISTANCE = "LEGAL_ASSISTANCE", _("Юридическая помощь")
     SOCIAL_ASSISTANCE = "SOCIAL_ASSISTANCE", _("Социальная помощь")
@@ -43,18 +56,31 @@ class HelpTypes(models.TextChoices):
 
 
 class Question(BaseModel):
-    question = models.CharField(max_length=200, verbose_name="Вопрос")
-    answer = models.CharField(max_length=3856, verbose_name="Ответ")
-    short_description = models.CharField(max_length=20, verbose_name="Короткое описание")
-    regions = models.ManyToManyField(Region,
-                                     related_name='questions',
-                                     blank=True,
-                                     verbose_name='Регионы')
+    """Assistance question model."""
+
+    question = models.CharField(
+        max_length=200,
+        verbose_name="Вопрос",
+    )
+    answer = models.CharField(
+        max_length=3856,
+        verbose_name="Ответ",
+    )
+    short_description = models.CharField(
+        max_length=20,
+        verbose_name="Короткое описание",
+    )
+    regions = models.ManyToManyField(
+        Region,
+        related_name="questions",
+        blank=True,
+        verbose_name="Регионы",
+    )
     question_type = models.CharField(
         max_length=100,
-        choices=HELP_TYPES,
-        default=LAW,
-        verbose_name='Тип вопроса'
+        choices=HelpTypes.choices,
+        default=HelpTypes.LEGAL_ASSISTANCE,
+        verbose_name="Тип вопроса",
     )
 
     class Meta:
@@ -64,14 +90,23 @@ class Question(BaseModel):
 
 
 class FundProgram(BaseModel):
-    title = models.CharField(max_length=200,
-                             unique=True,
-                             verbose_name='Название')
-    description = models.CharField(max_length=500, verbose_name='Описание')
-    regions = models.ManyToManyField(Region,
-                                     related_name='programs',
-                                     blank=True,
-                                     verbose_name='Регионы')
+    """Fund program model."""
+
+    title = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name="Название",
+    )
+    description = models.CharField(
+        max_length=500,
+        verbose_name="Описание",
+    )
+    regions = models.ManyToManyField(
+        Region,
+        related_name="programs",
+        blank=True,
+        verbose_name="Регионы",
+    )
 
     class Meta:
         verbose_name = "Программа фонда"
