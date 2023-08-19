508dc1076028bbd81333ba9ed3ddb39a8f235962
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-08-19 18:25:09 +0300

    make field region_key autogenerated

--- src/core/factories.py
+++ src/core/factories.py
@@ -1,21 +1,14 @@
-from factory import Faker, post_generation
+from factory import Faker
 from factory.django import DjangoModelFactory
-from transliterate import translit
 
 from core.models import Region
-from core.utils import to_snake_case
 
 
 class RegionFactory(DjangoModelFactory):
-    class Meta:
+    """Region model factory."""
+
+    class Meta:  # noqa
         model = Region
-        django_get_or_create = ('region_name', )
+        django_get_or_create = ("region_name",)
 
     region_name = Faker("region", locale="ru_RU")
-
-    @post_generation
-    def add_region_key(self, create, extracted, **kwargs):
-        """Add region key field to region instance before saving."""
-        translited_text = translit(self.region_name, reversed=True)
-        self.region_key = to_snake_case(translited_text)
-        self.save()
--- src/core/models.py
+++ src/core/models.py
@@ -1,24 +1,44 @@
 from django.db import models
+from transliterate import translit
+
+from core.utils import to_snake_case
 
 
 class BaseModel(models.Model):
+    """Base model."""
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
+    class Meta:  # noqa
         abstract = True
 
 
 class Region(BaseModel):
+    """Region model."""
 
     region_name = models.CharField(max_length=200, unique=True)
     region_key = models.CharField(max_length=200, unique=True)
 
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        """Save method."""
+        self.region_key = to_snake_case(
+            translit(self.region_name, reversed=True)
+        )
+        return super(Region, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
     def __str__(self):
         return self.region_name
 
-    class Meta:
+    class Meta:  # noqa
         verbose_name = "Регион"
         verbose_name_plural = "Регионы"
         ordering = ("region_name",)
