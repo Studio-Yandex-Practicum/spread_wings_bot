0209720ae397eec99088f718f1e017aa271d8bd1
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-08-20 13:16:38 +0300

    Тесты обновлены

--- src/tests/unit/test_handlers/conftest.py
+++ src/tests/unit/test_handlers/conftest.py
@@ -17,3 +17,23 @@ def context():
     context.bot.set_my_commands = AsyncMock(return_value=[])
     context.bot.set_chat_menu_button = AsyncMock(return_value=[])
     return context
+
+
+@pytest.fixture
+def mocked_reply_markup():
+    """Reply markup mock."""
+    return []
+
+
+@pytest.fixture
+def mocked_message_text():
+    """Message text mock."""
+    return "MESSAGE"
+
+
+@pytest.fixture
+def mocked_message(mocked_message_text):
+    """Message object mock."""
+    message = Mock()
+    message.value = mocked_message_text
+    return message
