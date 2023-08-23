6cf48eef252f5e496ed7038bb00ef86dfdb1f5f3
Алексей Кознов <alekseikoznov@yandex.ru>
2023-08-23 19:34:11 +0300

    refactor phone format

---
+++ src/bot/constants/validation.py
@@ -0,0 +1,5 @@
+TELEGRAM_USERNAME = r"^[\w\_]{5,32}$"
+PHONE = r"^[\+]?[7, 8][-\s\.]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{2}[-\s\.]?[0-9]{2}$"
+PHONE_FORMAT = "+7 777 777 77 77"
+TELEGRAM_FORMAT = "username"
+PHONE_PATTERN = "+7 (XXX) XXX-XX-XX"
