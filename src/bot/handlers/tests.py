5c6e5829e4df1b5b3167aa5f6addee4318c025e5
Ольга Жолудева <joludeva.olga@yandex.ru>
2023-08-23 09:04:27 +0300

    Add the states to the debug logger

---
+++ src/bot/handlers/tests.py
@@ -0,0 +1,15 @@
+# FUNCTIONS = {
+#     'States.ASSISTANCE.value': 'start',
+#     'States.ASSISTANCE_TYPE': 'select_type_of_help',
+#     'States.CONTACT_US': 'contact_with_us',
+#     'States.FUND_PROGRAMS': 'fund_programs',
+#     'States.QUESTIONS_AND_CONTACTS': 'select_assistance',
+#     'States.REGION': 'receive_assistance',
+#     'States.SHOW_CONTACT': 'show_contact',
+#     'States.QUESTION': 'ask_question',
+#     'States.NAME': 'ask_name',
+# }
+# query_data = 'ask_name'
+# keys = list(FUNCTIONS.keys())
+# index = list(FUNCTIONS.values()).index(query_data)
+# print(*keys[index].split('.')[1:2])
