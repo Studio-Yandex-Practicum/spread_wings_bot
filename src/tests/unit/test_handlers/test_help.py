8cc90747edad69559065e6552a42dedb6a01f452
bobr <borya2072@gmail.com>
2023-08-26 13:41:44 +0300

    fixed test_help test

--- src/tests/unit/test_handlers/test_help.py
+++ src/tests/unit/test_handlers/test_help.py
@@ -3,6 +3,9 @@ from unittest.mock import AsyncMock, patch
 import pytest
 
 from bot.handlers import main_handlers
+from bot.keyboards.assistance import (
+    to_the_original_state_and_previous_step_keyboard,
+)
 
 
 @pytest.mark.asyncio
@@ -18,4 +21,7 @@ async def test_help_handler(
         AsyncMock(return_value=mocked_message),
     ):
         await main_handlers.help_command(update, context)
-    update.message.reply_text.assert_called_once_with(mocked_message_text)
+    update.message.reply_text.assert_called_once_with(
+        mocked_message_text,
+        reply_markup=to_the_original_state_and_previous_step_keyboard[0],
+    )
