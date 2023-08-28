09144a1dbb9994b1d95adb3b49ad4ab078d2db68
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-08-28 19:01:35 +0300

    Добавлены утилиты для создания писем с ссылками на сброс пароля.

--- src/users/signals.py
+++ src/users/signals.py
@@ -1,31 +1,12 @@
-from django.contrib.auth.tokens import default_token_generator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.template.loader import render_to_string
-from django.urls import reverse
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
 
-from config import settings
 from users.models import User
+from utils.emailing.reset_password import send_password_reset_email
 
 
 @receiver(post_save, sender=User)
-def send_password_reset_email(sender, instance, created, **kwargs):
+def password_reset_email(sender, instance, created, **kwargs):
     """Send email to new admin with link to set password."""
-    if created and instance.is_superuser:
-        token_generator = default_token_generator
-        uid = urlsafe_base64_encode(force_bytes(instance.pk))
-        token = token_generator.make_token(instance)
-        reset_url = reverse("password_reset", args=[uid, token])
-
-        subject = "Password change Link"
-        message = render_to_string(
-            "registration/password_change_form.html",
-            {
-                "user": instance,
-                "reset_url": reset_url,
-            },
-        )
-        from_email = settings.EMAIL_HOST_USER
-        instance.email_user(subject, message, from_email)
+    if created and instance.is_staff and not instance.is_superuser:
+        send_password_reset_email(instance)
