e48b19d110d26acbbcebaaa5c1f555e3d97150e4
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-08-28 18:45:32 +0300

    Добавлена view-функция для обработки запросов на сброс пароля.

---
+++ src/users/views.py
@@ -0,0 +1,18 @@
+from django.contrib import messages
+from django.contrib.auth.views import PasswordResetConfirmView
+from django.urls import reverse_lazy
+
+
+class PasswordSetView(PasswordResetConfirmView):
+    """User password reset view."""
+
+    success_url = reverse_lazy("admin:index")
+    template_name = "password_set_confirm.html"
+
+    def form_valid(self, form):
+        """Set user satus as active if password was changed."""
+        response = super().form_valid(form)
+        messages.success(self.request, "Ваш пароль был успешно изменен.")
+        self.user.is_active = True
+        self.user.save()
+        return response
