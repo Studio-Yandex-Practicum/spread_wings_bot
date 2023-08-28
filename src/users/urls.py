0d12d7b6a3d794e1a0b93d7917e74a85b23f1592
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-08-28 18:54:03 +0300

    Добавлен путь для смены пароля.

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,1 +1,13 @@
-urlpatterns = []
+from django.urls import path
+
+from users.views import PasswordSetView
+
+app_name = "users"
+
+urlpatterns = [
+    path(
+        "set_password/<uidb64>/<token>/",
+        PasswordSetView.as_view(),
+        name="password_set",
+    ),
+]
