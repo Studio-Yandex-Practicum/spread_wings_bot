bad7f1e9ef58242a125c024ad6f42621fa4c77f1
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-08-29 21:32:38 +0300

    Удалена проверка условия is_staff.

--- src/users/signals.py
+++ src/users/signals.py
@@ -8,5 +8,5 @@ from utils.emailing.reset_password import send_password_reset_email
 @receiver(post_save, sender=User)
 def password_reset_email(sender, instance, created, **kwargs):
     """Send email to new admin with link to set password."""
-    if created and instance.is_staff and not instance.is_superuser:
+    if created and not instance.is_superuser:
         send_password_reset_email(instance)
