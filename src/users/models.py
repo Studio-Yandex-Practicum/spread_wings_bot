94c8d394b1227b505c8b4eab47d5048e097951e7
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-08-29 21:38:40 +0300

    Удалены ненужные настройки.

--- src/users/models.py
+++ src/users/models.py
@@ -38,21 +38,6 @@ class User(AbstractUser):
         verbose_name="Регион пользователя",
     )
 
-    is_staff = models.BooleanField(
-        _("staff status"),
-        default=True,
-        help_text=_(
-            "Designates whether the user can log into this admin site."
-        ),
-    )
-    is_active = models.BooleanField(
-        _("active"),
-        default=False,
-        help_text=_(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
-        ),
-    )
     objects = UserManager()
 
     USERNAME_FIELD = "email"
