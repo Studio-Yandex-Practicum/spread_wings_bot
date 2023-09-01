493ce79e2cb649a989515fb77d30adce814ac953
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-01 13:35:24 +0300

    Добавлен кастомный сайт админ-панели с 2FA.

---
+++ src/users/sites.py
@@ -0,0 +1,10 @@
+from django_otp.admin import OTPAdminSite
+
+from users.forms import CustomOTPAuthenticationForm
+
+
+class CustomOTPAdminSite(OTPAdminSite):
+    """Customized admin site."""
+
+    login_form = CustomOTPAuthenticationForm
+    login_template = "users/authentication/login.html"
