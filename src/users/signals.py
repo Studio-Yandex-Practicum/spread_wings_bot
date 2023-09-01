53d2aa1e0374666f71d23c24d0dbc5f01f77c2a3
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-01 13:33:10 +0300

    Добавлено создание модели EmailDevice новых пользователей для рассылки OTP.

--- src/users/signals.py
+++ src/users/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django_otp.plugins.otp_email.models import EmailDevice
 
 from users.models import User
 from users.utils.emailing.reset_password import send_password_reset_email
@@ -8,5 +9,10 @@ from users.utils.emailing.reset_password import send_password_reset_email
 @receiver(post_save, sender=User)
 def password_reset_email(sender, instance, created, **kwargs):
     """Send email to new admin with link to set password."""
-    if created and not instance.is_superuser:
-        send_password_reset_email(instance)
+    if created:
+        EmailDevice.objects.create(
+            user=instance,
+            email=instance.email,
+        )
+        if not instance.is_superuser:
+            send_password_reset_email(instance)
