74ab09efd472774e34ab879c28303cea31170f5d
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-01 13:40:19 +0300

    Добавлена форма для 2FA.

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.hashers import make_password
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext_lazy
+from django_otp.forms import OTPAuthenticationFormMixin
+from django_otp.plugins.otp_email.models import EmailDevice
 
 from users.models import User
 from users.utils.users.registration import generate_random_password
@@ -33,3 +38,44 @@ class UserChangeForm(forms.ModelForm):
             "is_active",
             "is_superuser",
         )
+
+
+class CustomOTPAuthenticationFormMixin(OTPAuthenticationFormMixin):
+    """Customized OTPAuthenticationFormMixin mixin."""
+
+    otp_error_messages = {
+        "token_required": _("Пожалуйста, введите одноразовый код."),
+        "challenge_exception": _("Ошибка генерации кода: {0}"),
+        "not_interactive": _("Код не может быть отправлен."),
+        "challenge_message": _("Код отправлен на указанную почту."),
+        "invalid_token": _("Неверный код. Проверьте правильность."),
+        "n_failed_attempts": ngettext_lazy(
+            "Допущено %(failure_count) ошибок. Доступ временно ограничен.",
+            "Допущено %(failure_count) ошибок. Доступ временно ограничен.",
+            "failure_count",
+        ),
+        "verification_not_allowed": _("Верификация недоступна"),
+    }
+
+    def _chosen_device(self, user):
+        """Return EmailDevise as default."""
+        return EmailDevice.objects.filter(user=user).first()
+
+
+class CustomOTPAuthenticationForm(
+    CustomOTPAuthenticationFormMixin, AuthenticationForm
+):
+    """Customized OTPAuthenticationForm form."""
+
+    otp_token = forms.CharField(
+        required=False, widget=forms.TextInput(attrs={"autocomplete": "off"})
+    )
+
+    otp_challenge = forms.CharField(required=False)
+
+    def clean(self):
+        """Clean form data."""
+        self.cleaned_data = super().clean()
+        self.clean_otp(self.get_user())
+
+        return self.cleaned_data
