2dd442c8f526f6d6a0e4d648cb664463088862e6
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-01 13:36:57 +0300

    Заменен дефолтный сайт админ-панели на кастомизированный.

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from users.sites import CustomOTPAdminSite
+
+admin.site.__class__ = CustomOTPAdminSite
+
 urlpatterns = [
     path("users/", include("users.urls"), name="users"),
     path("bot/", include("bot.urls"), name="bot"),
