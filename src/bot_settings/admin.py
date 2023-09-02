077be80459faa1904be9e50fb8f745f339435836
bobr <borya2072@gmail.com>
2023-09-02 13:44:36 +0300

    deleted filters for bot settings

--- src/bot_settings/admin.py
+++ src/bot_settings/admin.py
@@ -7,7 +7,6 @@ from bot_settings.models import BotSettings
 class BotSettingsAdmin(admin.ModelAdmin):
     """Base admin configuration for BotSettings model."""
 
-    list_display = ("get_title", "get_type", "get_value")
     list_filter = ("type",)
     search_fields = ("title", "type", "value")
     readonly_fields = ("type",)
@@ -16,18 +15,3 @@ class BotSettingsAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         """Disable delete permission for BotSettings model."""
         return False
-
-    @admin.display(description="Название настройки")
-    def get_title(self, obj):
-        """Display title of settings in admin panel."""
-        return obj.title[:100]
-
-    @admin.display(description="Тип значения")
-    def get_type(self, obj):
-        """Display type of settings in admin panel."""
-        return obj.get_type_display()[:100]
-
-    @admin.display(description="Значение настройки")
-    def get_value(self, obj):
-        """Display value of settings in admin panel."""
-        return obj.value[:100]
