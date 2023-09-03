c046405ccb5e0adb876ec0b48763b1ec71a773f3
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-04 00:27:56 +0300

    Изменена функция на проверку otp, отправка проводится автоматически.

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.hashers import make_password
+from django.db import transaction
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import ngettext_lazy
 from django_otp.forms import OTPAuthenticationFormMixin
@@ -56,6 +57,43 @@ class CustomOTPAuthenticationFormMixin(OTPAuthenticationFormMixin):
         ),
         "verification_not_allowed": _("Верификация недоступна"),
     }
+
+    def clean_otp(self, user):
+        """Perform OTP clean."""
+        if user is None:
+            return
+
+        validation_error = None
+        with transaction.atomic():
+            try:
+                device = self._chosen_device(user)
+                token = self.cleaned_data.get("otp_token")
+
+                user.otp_device = None
+
+                try:
+                    if self.cleaned_data.get("otp_challenge"):
+                        self._handle_challenge(device)
+                    elif token:
+                        user.otp_device = self._verify_token(
+                            user, token, device
+                        )
+                    else:
+                        self._handle_challenge(device)
+                        raise forms.ValidationError(
+                            self.otp_error_messages["token_required"],
+                            code="token_required",
+                        )
+                finally:
+                    if user.otp_device is None:
+                        self._update_form(user)
+            except forms.ValidationError as e:
+                # Validation errors shouldn't abort the transaction, so we have
+                # to carefully transport them out.
+                validation_error = e
+
+        if validation_error:
+            raise validation_error
 
     def _chosen_device(self, user):
         """Return EmailDevise as default."""
